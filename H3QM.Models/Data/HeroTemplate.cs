f470161006d05d57417210fd0a2f10efe239131a
Petr Bogoyavlenskiy <Petr.Bogoyavlenskiy@lexomi.com>
2018-03-20 03:09:58 +0700

    1

---
+++ H3QM.Models/Data/HeroTemplate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace H3QM.Models.Data
+{
+    public class HeroTemplate
+    {
+        #region C-tor & Properties
+
+        public string Name { get; }
+
+        public string NewName { get; }
+
+        public string Icon { get; }
+
+        public string NewIcon { get; }
+
+        public byte[] OriginalPattern { get; }
+
+        public byte[] OriginalMapEdPattern { get; }
+
+        public byte[] ModifiedPattern { get; }
+
+        public byte[] ModifiedMapEdPattern { get; }
+
+        public HeroTemplate(string name, string newName, string icon, string newIcon, byte[] originalPattern, byte[] modifiedPattern, byte[] originalMapEdPattern, byte[] modifiedMapEdPattern)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            if (string.IsNullOrWhiteSpace(Name)) throw new ArgumentNullException(nameof(name));
+
+            NewName = newName ?? throw new ArgumentNullException(nameof(newName));
+            if (string.IsNullOrWhiteSpace(NewName)) NewName = Name;
+
+            Icon = icon ?? throw new ArgumentNullException(nameof(icon));
+            if (string.IsNullOrWhiteSpace(Icon)) throw new ArgumentNullException(nameof(icon));
+
+            NewIcon = newIcon ?? throw new ArgumentNullException(nameof(newIcon));
+            if (string.IsNullOrWhiteSpace(NewIcon)) NewIcon = Icon;
+
+            OriginalPattern = originalPattern ?? throw new ArgumentNullException(nameof(originalPattern));
+            OriginalMapEdPattern = originalMapEdPattern ?? throw new ArgumentNullException(nameof(originalMapEdPattern));
+
+            ModifiedPattern = modifiedPattern ?? throw new ArgumentNullException(nameof(modifiedPattern));
+            ModifiedMapEdPattern = modifiedMapEdPattern ?? throw new ArgumentNullException(nameof(modifiedMapEdPattern));
+        }
+
+        #endregion
+    }
+}
---
+++ H3QM.RunApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using H3QM.Interfaces.Services;
+using H3QM.Models.Data;
+using H3QM.Models.Enums;
+using H3QM.Services;
+
+namespace H3QM.RunApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IChangeExeService exeService = new ChangeExeService();
+            var orrin = exeService.ChangeHero(@"X:\Games\HoMM3X\h3hota_maped.exe", MarkerEnum.MapEditorMarker, KnownHero.Orrin.OriginalPattern, KnownHero.Orrin.ModifiedPattern);
+
+            Console.WriteLine("[EXE] Orrin: " + (orrin ? " OK!" : "NOT CHANGED!"));
+
+            ILodArchiveService lodArchiveService = new LodArchiveService();
+            var files = lodArchiveService.GetFiles(@"X:\Games\HoMM3X\Data\H3bitmap.lod", out var _);
+
+            var sfiles = files.Where(q => q.Name.Equals(KnownHero.Orrin.Icon)).ToArray();
+
+            //File.WriteAllBytes($@"X:\{KnownHero.Orrin.Icon}.bmp", Encoding.GetEncoding(1251).GetBytes(file.OriginalContent));
+
+            Console.ReadLine();
+        }
+    }
+}
